23c206c8957539b8b3ebdc0cfa2f5abc9e31b957
TPSang <124275402+TPSang@users.noreply.github.com>
2023-02-16 22:50:54 +0700

    category

--- MultiShop2/src/main/java/com/poly/main/Controller/CategoryController.java
+++ MultiShop2/src/main/java/com/poly/main/Controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.poly.main.Controller;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,25 +11,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.poly.main.Service.CategoryService;
+
 import com.poly.main.BaseResponse.BaseResponse;
 import com.poly.main.Entity.Category;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+
 	@Autowired
 	private CategoryService categoryService;
 
-	@GetMapping("/getAll")
-	public ResponseEntity<BaseResponse> getAll() {
+	@PostMapping("/category/index")
+	public String index() {
+		
+		return "redirect:/manager/category";
+	}
+	
+	@GetMapping("/index")
+	public String getAll(Model model) {
 		BaseResponse baseResponse = new BaseResponse();
 		try {
-			return ResponseEntity.ok().body(categoryService.getAll());
+			model.addAttribute("getAll",categoryService.getAll());
+			System.out.println(categoryService.getAll());
+			return "redirect:/manager/category";
 		} catch (Exception e) {
-			// TODO: handle exception
 			baseResponse.setCode("-1");
 			baseResponse.setMessage("Failed to find data");
-			return ResponseEntity.badRequest().body(baseResponse);
+			model.addAttribute("getAll", baseResponse);
+			return "redirect:/manager/category";
 		}
 
 	}
--- MultiShop2/src/main/java/com/poly/main/Repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poly.main.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.poly.main.Entity.UserRole;
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-
-}
